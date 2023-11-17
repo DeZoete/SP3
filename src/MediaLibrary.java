3454ff382fd28e24bc7afffe752f54c1fe507a26
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2023-11-17 21:52:10 +0100

    MediaLibrary getGenreList

--- src/MediaLibrary.java
+++ src/MediaLibrary.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class MediaLibrary {
+
+    FileIO io;
+    ArrayList<String> mediaGenres = new ArrayList<>();
+    ArrayList<Media> allMedia = new ArrayList<>();
+    ArrayList<Media> allMovies = new ArrayList<>();
+    ArrayList<Media> allSeries = new ArrayList<>();
+
+
+    ArrayList<Media> getGenreList(ArrayList<Media> mediaList, String genre){
+        ArrayList<Media> genreList = new ArrayList<>();
+
+        for (Media m : mediaList) {
+            if((m.getGenre).equals(genre)){
+                genreList.add(m);
+            }
+        }
+
+        return genreList;
+    }
+
+
+
 }
