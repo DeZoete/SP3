755615a35e665f0b6fcd332acd4e23d06b9515d6
Marcus-Ekeroth <cph-me313@cphbusiness.dk>
2023-11-21 12:16:31 +0100

    start menu improvements

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -41,97 +41,16 @@ public class StreamingService {
         String input = ui.getInput();
         if(input.equals("2")){
 
-            ui.displayMessage("Please enter a username.");
-            String userInput = ui.scan.nextLine();
-            ui.displayMessage("Please enter a password.");
-            String passwordInput = ui.scan.nextLine();
-            User user = new User(userInput, passwordInput, false);
-            users.add(user);
-            ui.displayMessage("Thank you for signing up , " + userInput + ".");
-            try{
-
-                FileWriter writer = new FileWriter("src/userdata.txt",true);
-                for(User s: users){
-                    String userSave = s.getUsername() + "," + s.getPassword() + "\n";
-                    writer.write(userSave);
-
-                }
-                writer.close();
-
-
-
-
-            } catch(IOException e){
-                ui.displayMessage("User not found. Press 1 to redirect to start menu.");
-                if(ui.getInput().equals("1")) {
-
-                    startMenu();
-
-                }
-
-            }
-
-
+            signUp();
 
         } else if(input.equals("1")){
 
-            ui.displayMessage("Please enter your username.");
-            String userInput = ui.getInput();
-            ui.displayMessage("Please enter your password.");
-            String passwordInput = ui.getInput();
-            //User userSignIn = new User(userInput, passwordInput, false);
-            try {
-                Scanner scan = new Scanner(userFile);
-                boolean loggingin = false;
-                for (int i = 0; scan.hasNextLine(); i++) {
-                    String split = scan.nextLine();
-                    String[] usersAndPasswords = split.split(",");
-                    username = usersAndPasswords[0];
-                    password = usersAndPasswords[1];
-                    if (username.equals(userInput) && password.equals(passwordInput)) {
-
-                            ui.displayMessage("Logging in. Stand by.");
-                            loggingin=true;
-                        }
-
-
-                    }
-
-                if(!loggingin) {
-                    ui.displayMessage("Username or password incorrect. Press 1 to be redirected to the start menu.");
-                    if (ui.getInput().equals("1")) {
-
-                        startMenu();
-
-                    }
-                }
-            } catch (FileNotFoundException e){
-                System.out.println("File not found.");
-            }
-
-                    /*if(users.contains(userSignIn)){
-                        ui.displayMessage("Logging in. Stand by.");
-
-                    }*/
-                    /*for(User c: users){
-                        if(c.equals(userSignIn)){
-                            ui.displayMessage("Logging in. Stand by.");
-                        }
-                    }*/
-                    /*if(users.contains(userSignIn)){
-                        ui.displayMessage("Please enter your password.");
-                        if(users.contains(ui.getInput())){
-                            ui.displayMessage("Logging in. Stand by.");*/
-
-
+            logIn();
 
         } else{
-            ui.displayMessage("Your input was invalid. Press 1 to be redirected to the start menu.");
-            if(ui.getInput().equals("1")) {
 
-                startMenu();
+            invalidInput();
 
-            }
         }
 
     }
@@ -143,9 +62,93 @@ public class StreamingService {
     }
     private void signUp(){
 
+        ui.displayMessage("Please enter a username.");
+        String userInput = ui.scan.nextLine();
+        ui.displayMessage("Please enter a password.");
+        String passwordInput = ui.scan.nextLine();
+        User user = new User(userInput, passwordInput, false);
+        users.add(user);
+        ui.displayMessage("Thank you for signing up , " + userInput + ".");
+        try{
+
+            FileWriter writer = new FileWriter("src/userdata.txt",true);
+            for(User s: users){
+                String userSave = s.getUsername() + "," + s.getPassword() + "\n";
+                writer.write(userSave);
+
+            }
+            writer.close();
+
+
+
+
+        } catch(IOException e){
+            ui.displayMessage("User not found. Press 1 to redirect to start menu.");
+            if(ui.getInput().equals("1")) {
+
+                startMenu();
+
+            }
+
+        }
 
     }
     private void logIn(){
+        ui.displayMessage("Please enter your username.");
+        String userInput = ui.getInput();
+        ui.displayMessage("Please enter your password.");
+        String passwordInput = ui.getInput();
+        //User userSignIn = new User(userInput, passwordInput, false);
+        try {
+            Scanner scan = new Scanner(userFile);
+            boolean loggingin = false;
+            for (int i = 0; scan.hasNextLine(); i++) {
+                String split = scan.nextLine();
+                String[] usersAndPasswords = split.split(",");
+                username = usersAndPasswords[0];
+                password = usersAndPasswords[1];
+                if (username.equals(userInput) && password.equals(passwordInput)) {
 
+                    ui.displayMessage("Logging in. Stand by.");
+                    loggingin=true;
+                }
+
+
+            }
+
+            if(!loggingin) {
+
+                invalidUserPass();
+
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+    }
+
+    private void invalidInput(){
+
+        ui.displayMessage("Your input was invalid. Press 1 to be redirected to the start menu.");
+        if(ui.getInput().equals("1")) {
+
+            startMenu();
+
+        } else{
+
+            invalidInput();
+
+        }
+    }
+
+    private void invalidUserPass(){
+
+        ui.displayMessage("Username or password incorrect. Press 1 to be redirected to the start menu.");
+        if (ui.getInput().equals("1")) {
+
+            startMenu();
+
+        } else{
+            invalidInput();
+        }
     }
 }
