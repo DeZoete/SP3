99a11ce97d06feb6a4ece7152e1844f22eccc70a
Freddo-404 <cph-fd101@cphbusiness.dk>
2023-11-21 15:15:37 +0100

    Changes by mongus

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -33,65 +33,20 @@ public class StreamingService {
                 "Please choose one of the following options:" + "\n" +
                 "1. Sign in to an existing user \n" +
                 "2. Create a new user");
-        try {
-            Scanner scan = new Scanner(userFile);
-            for (int i = 0; scan.hasNextLine(); i++) {
-                String split = scan.nextLine();
-                String[] usersAndPasswords = split.split(",");
-                String username = usersAndPasswords[0];
-                String password = usersAndPasswords[1];
-                User user = new User(username, password, false);
-            }
-        }
-        catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }
+        io.writeUserData();
         String input = ui.getInput();
         if(input.equals("2")){
 
-            ui.displayMessage("Please enter a username.");
-            String userInput = ui.scan.nextLine();
-            ui.displayMessage("Please enter a password.");
-            String passwordInput = ui.scan.nextLine();
-            User user = new User(userInput, passwordInput, false);
-            users.add(user);
-            ui.displayMessage("Thank you for signing up , " + userInput + ".");
-            try{
-
-                FileWriter writer = new FileWriter("src/userdata.txt",true);
-                for(User s: users){
-                    String userSave = s.getUsername() + "," + s.getPassword() + "\n";
-                    writer.write(userSave);
-
-                }
-                writer.close();
-
-
-
-
-            } catch(IOException e){
-                ui.displayMessage("User not found. Press 1 to redirect to start menu.");
-                if(ui.getInput().equals("1")) {
-
-                    startMenu();
-
-                }
-
-            }
-
-
+            signUp();
 
         } else if(input.equals("1")){
 
             logIn();
 
         } else{
-            ui.displayMessage("Your input was invalid. Press 1 to be redirected to the start menu.");
-            if(ui.getInput().equals("1")) {
 
-                startMenu();
+            invalidInput();
 
-            }
         }
 
     }
@@ -150,10 +105,69 @@ searchMedia();
     }
     private void signUp(){
 
+        ui.displayMessage("Please enter a username.");
+        String userInput = ui.scan.nextLine();
+        ui.displayMessage("Please enter a password.");
+        String passwordInput = ui.scan.nextLine();
+        User user = new User(userInput, passwordInput, false);
+        users.add(user);
+        ui.displayMessage("Thank you for signing up , " + userInput + ".");
+        try{
+
+            FileWriter writer = new FileWriter("src/userdata.txt",true);
+            for(User s: users){
+                String userSave = s.getUsername() + "," + s.getPassword() + "\n";
+                writer.write(userSave);
+
+            }
+            writer.close();
+
+
+
+
+        } catch(IOException e){
+            ui.displayMessage("User not found. Press 1 to redirect to start menu.");
+            if(ui.getInput().equals("1")) {
+
+                startMenu();
+
+            }
+
+        }
 
     }
     private void logIn(){
 
+        ui.displayMessage("Please enter your username.");
+        String userInput = ui.getInput();
+        ui.displayMessage("Please enter your password.");
+        String passwordInput = ui.getInput();
+        //User userSignIn = new User(userInput, passwordInput, false);
+        try {
+            Scanner scan = new Scanner(userFile);
+            boolean loggingin = false;
+            for (int i = 0; scan.hasNextLine(); i++) {
+                String split = scan.nextLine();
+                String[] usersAndPasswords = split.split(",");
+                username = usersAndPasswords[0];
+                password = usersAndPasswords[1];
+                if (username.equals(userInput) && password.equals(passwordInput)) {
+
+                    ui.displayMessage("Logging in. Stand by.");
+                    loggingin=true;
+                    mainMenu();
+                }
+
+
+            }
+
+            if(!loggingin) {
+
+                invalidUserPass();
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not found.");
+        }
     }
     private void initializeLibrary(){
         movies = library.getAllMovies();
