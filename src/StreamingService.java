c577036722d1018cf146238b131b39d31a1e992f
Freddo-404 <cph-fd101@cphbusiness.dk>
2023-11-23 14:00:55 +0100

    En masse rettelser i diverse search functioner

--- src/StreamingService.java
+++ src/StreamingService.java
@@ -31,7 +31,7 @@ public class StreamingService {
     private FileIO io = new FileIO();
     private MediaLibrary library = new MediaLibrary();
 
-    public void startMenu(){
+    public void startMenu() {
 
         ui.displayMessage("Hello and welcome to Streamingservice! \n" +
                 "\n" +
@@ -40,30 +40,31 @@ public class StreamingService {
                 "2. Create a new user");
         users = io.readUserData("src/userdata.txt");
         String input = ui.getInput();
-       switch(input){
-           case "1":
-               logIn();
-               break;
-           case "2":
-               signUp();
-               break;
-           default:
-               invalidInput();
-               break;
-       }
+        switch (input) {
+            case "1":
+                logIn();
+                break;
+            case "2":
+                signUp();
+                break;
+            default:
+                invalidInput();
+                break;
+        }
 
     }
-    public void mainMenu(){
 
-       initializeLibrary();
+    public void mainMenu() {
 
-        ui.displayMessage("Please select your desired option from the menu below\n"+"\n"+
-                "1. Search for media"+"\n"+
-                "2. Find media sorted by genre"+ "\n"+
-                "3. Find media sorted by rating"+ "\n"+
-                "4. Show plan to watch list"+"\n"+
-                "5. Show media history"+"\n"+
-                "\n"+"6. Log out");
+        initializeLibrary();
+
+        ui.displayMessage("Please select your desired option from the menu below\n" + "\n" +
+                "1. Search for media" + "\n" +
+                "2. Find media sorted by genre" + "\n" +
+                "3. Find media sorted by rating" + "\n" +
+                "4. Show plan to watch list" + "\n" +
+                "5. Show media history" + "\n" +
+                "\n" + "6. Log out");
 
         String input = ui.getInput();
         switch (input) {
@@ -94,90 +95,95 @@ public class StreamingService {
         }
 
 
-        }
+    }
 
-    public void playMedia(Media m){
+    public void playMedia(Media m) {
         m.play();
         currentUser.addWatchedList(m);
         mainMenu();
     }
 
-    public void searchMedia(){
+    public void searchMedia() {
 
-        ui.displayMessage("Search for the title you want to watch"+"\n"+"9. Go to main menu"+"\n");
+        ui.displayMessage("Search for the title you want to watch" + "\n" + "0. Go to main menu" + "\n");
 
         String input = ui.getInput();
-        if(!input.equals("9")){
-        ArrayList<Media>results = new ArrayList<>();
-        HashSet<String> uniqueList = new HashSet<>(); //Der kan kun være en af hver
-        for (Media m : media) {
-            String title = m.getTitel();
-            if (title.toLowerCase().contains(input) && uniqueList.add(title.toLowerCase())) {
-                results.add(m);
+        if (!input.equals("0")) {
+            ArrayList<Media> results = new ArrayList<>();
+            HashSet<String> uniqueList = new HashSet<>(); //Der kan kun være en af hver
+            for (Media m : media) {
+                String title = m.getTitel();
+                if (title.toLowerCase().contains(input) && uniqueList.add(title.toLowerCase())) {
+                    results.add(m);
+                }
             }
-        }
-            for(Media r : results ){
-            System.out.println(r);
-        }
-    } else if(input.equals("9")){
+            for (Media r : results) {
+                System.out.println(r);
+            }
+        } else if (input.equals("0")) {
             mainMenu();
         }
     }
-
     public void searchGenre() {
-        ui.displayMessage("Type in your genre you want to find" + "\n" + "9. Go to main menu" + "\n");
+        ui.displayMessage("Type in your genre you want to find" + "\n" + "0. Go to main menu" + "\n");
+
         System.out.println(library.getMovieGenres());
         String input = ui.getInput();
-        if (!input.equals("9")) {
+        if (!input.equals("0")) {
             currentList = library.makeGenreList(media, input);
             ui.displayArrayList(currentList);
 
+        } else if (input.equals("0")) {
+            mainMenu();
+        } else{
+            ui.displayMessage("please enter a correct genre");
+            searchGenre();
+        }
     }
 
     public void searchRating() {
-        ui.displayMessage("Sort by rating. What is the minimum rating media should have?" + "\n");
+       String input = String.valueOf(ui.getNumericInput("Sort by rating. What is the minimum rating media should have?" + "\n" + "0. Go to main menu" + "\n"));
 
-        String input = ui.getInput();
 
-        //Dette kan gå galt hvis bruger ikke skriver en float
-        float rating = Float.parseFloat(input);
+        if (!input.equals("0")) {
+            //Dette kan gå galt hvis bruger ikke skriver en float
+            float rating = Float.parseFloat(input);
+            currentList = library.makeMinimumRatingList(media, rating);
+            ui.displayArrayList(currentList);
 
-        currentList = library.makeMinimumRatingList(media,rating);
-        System.out.println(currentList);
-
-        } else if (input.equals("9")) {
+        } else if (input.equals("0")) {
             mainMenu();
         }
     }
+
+
 
 
 
     public void showHistory() {
-        if(currentUser.getWatchedList()==null){
+        if (currentUser.getWatchedList() == null) {
             ui.displayMessage("Your media history list is empty. Go watch some media :)");
             mainMenu();
-        }else {
-            currentList=currentUser.getWatchedList();
+        } else {
+            currentList = currentUser.getWatchedList();
             ui.displayArrayList(currentList);
             mainMenu();
         }
     }
 
     public void showToWatchlist() {
-        if(currentUser.getToWatchList()==null){
+        if (currentUser.getToWatchList() == null) {
             ui.displayMessage("Your plan to watch list is empty. When you find media you can add it to your plan to watch list");
             mainMenu();
-        }else {
-            currentList=currentUser.getToWatchList();
+        } else {
+            currentList = currentUser.getToWatchList();
             ui.displayArrayList(currentList);
             mainMenu();
         }
     }
 
 
-
-
-    private void signUp(){
+    private void signUp() {
 
         ui.displayMessage("Please enter a username.");
         String userInput = ui.scan.nextLine();
@@ -191,10 +197,10 @@ public class StreamingService {
 
         startMenu();
 
-        }
+    }
 
 
-    private void logIn(){
+    private void logIn() {
 
         ui.displayMessage("Please enter your username.");
         String userInput = ui.getInput();
@@ -202,7 +208,7 @@ public class StreamingService {
         String passwordInput = ui.getInput();
         User userSignIn = new User(userInput, passwordInput, false);
         boolean loggingin = false;
-        for(User c: users) {
+        for (User c : users) {
 
             if (userInput.equals(c.getUsername()) && passwordInput.equals(c.getPassword())) {
                 ui.displayMessage("Logging in. Stand by.");
@@ -213,80 +219,80 @@ public class StreamingService {
         }
 
 
-            if(!loggingin){
-                invalidUserPass();
-            }
+        if (!loggingin) {
+            invalidUserPass();
         }
+    }
 
 
-
-    private void initializeLibrary(){
+    private void initializeLibrary() {
         movies = library.getAllMovies();
         series = library.getAllSeries();
         media = library.getAllMedia();
     }
 
-    private void invalidInput(){
+    private void invalidInput() {
 
         ui.displayMessage("Your input was invalid. Press 1 to be redirected to the start menu.");
-        if(ui.getInput().equals("1")) {
+        if (ui.getInput().equals("1")) {
 
             startMenu();
 
-        } else{
+        } else {
             invalidInput();
         }
     }
 
-    private void invalidUserPass(){
+    private void invalidUserPass() {
 
         ui.displayMessage("Your username or password is invalid. Press 1 to be redirected to the start menu.");
-        if(ui.getInput().equals("1")) {
+        if (ui.getInput().equals("1")) {
 
             startMenu();
 
-        } else{
+        } else {
             invalidInput();
         }
     }
-    private void mediaChoice(Media media){
-        ui.displayMessage("1. Play "+media.getTitel()+"\n"+
-                "2. Add to watch later"+"\n"+
+
+    private void mediaChoice(Media media) {
+        ui.displayMessage("1. Play " + media.getTitel() + "\n" +
+                "2. Add to watch later" + "\n" +
                 "3. Go back to main menu");
         String input = ui.getInput();
-      switch (input){
-          case"1":
-          playMedia(media);
-          break;
-          case"2":
-              currentUser.addToWatchList(media);
-              mediaChoice(media);
-              break;
-          case"3":
-              mainMenu();
-              break;
-          default:
-             invalidInput();
-      }
+        switch (input) {
+            case "1":
+                playMedia(media);
+                break;
+            case "2":
+                currentUser.addToWatchList(media);
+                mediaChoice(media);
+                break;
+            case "3":
+                mainMenu();
+                break;
+            default:
+                invalidInput();
+        }
     }
 
-    private Media pickMedia(ArrayList<Media> list){
+    private Media pickMedia(ArrayList<Media> list) {
         ui.displayMessage("Pick media from the list above by writing a titel.");
         String input = ui.getInput();
         Boolean picked = false;
         Media pickedMedia;
         for (Media m : list) {
-            if(input.equalsIgnoreCase(m.getTitel())){
-                picked=true;
-                pickedMedia= m;
+            if (input.equalsIgnoreCase(m.getTitel())) {
+                picked = true;
+                pickedMedia = m;
                 return pickedMedia;
             }
+
         }
 
         ui.displayMessage("The titel you wrote doesn't match your list. Try again.");
         return pickMedia(list);
     }
-
 
 
 }
